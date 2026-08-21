3e6d5ca2fd965c2554c9e4cb8b188359a29b7c25
Davi Ortiz - Scientist <daviortiz.anhanguera@gmail.com>
2026-08-21 19:49:34 -0300

    Update and rename Ola Java to Ola_Java.java

---
+++ Aula01/Ola_Java.java
@@ -0,0 +1,19 @@
+
+
+public class Ola {                               // Toda public class precisa ser o nome do arquivo;
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo!");
+		
+		System.out.println("\n 1) Foi compilado corretamente;");
+		
+		System.out.println("\n 2) Exercicio da Ficha \n");
+		
+		Ficha.main();
+		
+		System.out.println("\n 3) Exercicio da Erros \n");
+		
+		Erros.main();
+	}
+}
+
+
