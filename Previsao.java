c895a80d90e69ab007596eb0ec3977381bb607b2
Davi Ortiz <daviortiz.anhanguera@gmail.com>
2026-08-22 16:54:08 -0300

    add atividade 3 exercicio aula02

---
+++ Previsao.java
@@ -0,0 +1,12 @@
+public class Previsao {
+    public static void main(String[] args) {
+
+        System.out.println(7 / 2);          // resultado: 3, pois é uma divisão inteira
+        System.out.println(7 / 2.0);        // resultado: 3.5, pois é uma divisão de ponto flutuante
+        System.out.println(7.0 / 2);        // resultado: 3.5, pois é uma divisão de ponto flutuante
+        System.out.println(7 % 2);          // resultado: 1, pois é o resto da divisão inteira  
+        System.out.println((int) 7.9);      // resultado: 7, pois é uma conversão de tipo (casting)
+        System.out.println(10 / 4 * 4);     // resultado: 10, pois é uma divisão inteira seguida de multiplicação
+
+    }
+}
